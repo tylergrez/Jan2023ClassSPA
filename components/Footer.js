a9375765dc971f1fc10bb3d1e06d57dcd09a7b1a
tylergrez <tyler.grzegorek@gmail.com>
2023-02-20 17:41:02 -0700

    class spa push

---
+++ components/Footer.js
@@ -0,0 +1,8 @@
+import html from "html-literal";
+
+export default () => html `<footer>
+&copy; 2020 <a href="https://savvycoders.com/">Savvy Coders</a>
+</footer>`;
+// one-line command to export the anonymous function, the "html" in front of the literal simply allows us to view
+// the code as HTML rather than blanket burnt orange color
+// the () establishes an anonymous function to be called in index.js
